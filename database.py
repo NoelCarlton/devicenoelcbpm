50be15c66c9824b7a3b9528bba7ed668fd0be13f
NoelCarlton <tonight.26@hotmail.com>
2019-06-20 18:06:10 +0800

    删除数据库访问信息

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData, Table
 
-engine = create_engine("mysql://device:'ZC2019@bi.cbpm'@192.168.31.63:3306/train",convert_unicode=True)
+engine = create_engine("",convert_unicode=True)
 metadata = MetaData(bind=engine)
 
 drivers = Table('driver', metadata, autoload=True)
