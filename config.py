97400c8a7b5e55c174a4bd955da329c3575525da
NoelCarlton <tonight.26@hotmail.com>
2019-06-20 18:06:39 +0800

    删除数据库访问信息

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')  # 模板文件的路径
 
 STATICFILES_DIR = os.path.join(BASE_DIR, 'static')  # 静态文件的路径
 
-SQLALCHEMY_DATABASE_URI = "mysql://device:ZC2019@bi.cbpm@192.168.31.63:3306/train"  # 数据库URI
+SQLALCHEMY_DATABASE_URI = ""  # 数据库URI
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False  # 查询跟踪
